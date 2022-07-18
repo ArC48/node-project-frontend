d92a90585bfef71601ed3507bf7c0aad4eea9765
ArC48 <beqa3352@gmail.com>
2022-07-18 13:08:16 +0400

    feature/add: POST request logic added

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ const fetchWithoutBody = async (method, id) => {
   });
 };
 
+// fetch function with body
+const fetchWithBody = async (method, body, id) => {
+  const hasId = id ? `${apiBaseUrl}/${id}` : apiBaseUrl;
+  return fetch(hasId, {
+    method,
+    headers,
+    body: JSON.stringify(body),
+  });
+};
+
 const createExpenseHTML = (expense) => {
   let expensesTotal = 0;
 
@@ -61,10 +71,65 @@ const createExpenseHTML = (expense) => {
   });
 };
 
+// POST request logic
+const expenseSampleCreate = async () => {
+  const storeInput = document.getElementById('storeInput');
+  const priceInput = document.getElementById('priceInput');
+  const errorTexts = document.getElementById('errors');
+  errorTexts.innerText = '';
+  const errors = [];
+
+  if (priceInput.value[0] === '0') {
+    errors.push('Price must be at least 1');
+  }
+  if (!storeInput.value || !priceInput.value) {
+    errors.push('Input Fields Can\'t be Empty!');
+  }
+  if (Number.isNaN(Number(priceInput.value))) {
+    errors.push('Price must be a Number!');
+  }
+  if (priceInput.value < 0) {
+    errors.push('Price must be a positive number!');
+  }
+
+  if (errors.length) {
+    errors.forEach((err) => {
+      errorTexts.innerHTML += `<li>${err}</li>`;
+    });
+  } else {
+    try {
+      const result = await fetchWithBody('POST', {
+        text: storeInput.value,
+        price: Number(priceInput.value),
+      });
+
+      const expense = await result.json();
+      createExpenseHTML(expense);
+
+      storeInput.value = '';
+      priceInput.value = '';
+    } catch (err) {
+      errors.style.display = 'block';
+      errors.innerHTML = `<li>${err}</li>`;
+    }
+  }
+};
+
 const render = async () => {
+  const addButton = document.getElementById('addButton');
+  addButton.addEventListener('click', expenseSampleCreate);
+
   const fetchedData = await fetchWithoutBody('GET');
   const fetchedExpenses = await fetchedData.json();
-  createExpenseHTML(fetchedExpenses);
+
+  if (fetchedExpenses.length) {
+    createExpenseHTML(fetchedExpenses);
+  } else {
+    document.getElementById('errors').style.display = 'block';
+    document.getElementById('errors').innerText = 'No Expenses in the container!';
+    document.getElementById('totalAmountId').innerText = 'Total: $0';
+    createExpenseHTML(fetchedExpenses);
+  }
 };
 
 window.onload = () => {
